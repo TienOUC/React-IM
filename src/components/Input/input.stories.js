43c782b67a312d9f158324fb6caf4119a2b596de
tienouc <willtien@outlook.com>
2020-07-02 22:58:26 +0800

    feat: search

---
+++ src/components/Input/input.stories.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Input from '.';
+import Icon from 'components/Icon';
+import { ReactComponent as ClipIcon } from 'assets/icons/clip.svg'
+import { ReactComponent as PlanIcon } from 'assets/icons/plane.svg'
+
+export default {
+    title: 'UI组件/ Input',
+    component: Input
+}
+
+export const Default = () => {
+    return <Input />
+}
+
+export const Search = () => {
+    return <Input.Search />
+}
+
+export const WithAffix = () => {
+    return (
+        <div>
+            <Input
+                prefix={<Icon icon={ClipIcon} color='#ccc' />}
+                suffix={<Icon icon={PlanIcon} color='#ccc' />}
+            />
+        </div>
+    )
+}
--- src/theme.js
+++ src/theme.js
@@ -4,5 +4,10 @@ export default {
     gray: 'rgba(24, 28, 47, .2)',
     red: '#f34848',
     darkPurple: '#292f4c',
-    normal: '1.4rem'
+    gray2: 'rgba(241, 237, 237, .3)',
+    gray3: 'rgba(24, 28, 47, .3)',
+    grayDark: '#181c2f',
+
+    normal: '1.4rem',
+    medium: '1.6rem'
 }
