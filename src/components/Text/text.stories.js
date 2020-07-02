341b47ed53963c2a4b75230b22e1163ebf7f92d3
tienouc <willtien@outlook.com>
2020-07-02 23:35:36 +0800

    feat: text

---
+++ src/components/Text/text.stories.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import Text from '.';
+
+export default {
+    title: '排版/ Text',
+    component: Text
+}
+
+export const Default = () => {
+    return <Text> Default </Text>
+}
+
+export const Secondary = () => {
+    return <Text type='secondary'> secondary文本 </Text>
+}
+
+export const Medium = () => {
+    return <Text type='medium'> medium文本 </Text>
+}
+
+export const LargeAndBold = () => {
+    return <Text size='large' bold> large文本, 加粗 </Text>
+}
+
