34057d7e0e0f1e6ed33dae039f1209c2e8b60560
tienouc <willtien@outlook.com>
2020-07-30 14:18:24 +0800

    github-pages

---
+++ build/precache-manifest.81fb16e5a1b31731d714629f215ae582.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e673681cf81d2d7015465e23b8c6703",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4aa541602e0d5043c27",
+    "url": "./static/css/main.86b25147.chunk.css"
+  },
+  {
+    "revision": "fdc18c905b0560a56167",
+    "url": "./static/js/2.b3bb8ae3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b3bb8ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4aa541602e0d5043c27",
+    "url": "./static/js/main.582c5c8d.chunk.js"
+  },
+  {
+    "revision": "16671eae8c676093c1e8",
+    "url": "./static/js/runtime-main.a58ab876.js"
+  },
+  {
+    "revision": "4eb0a6538a69e1698c7988089a8ad6d0",
+    "url": "./static/media/arrowMenuLeft.4eb0a653.svg"
+  },
+  {
+    "revision": "f9abbe91a3da1a97d72410561493e1aa",
+    "url": "./static/media/arrowRight.f9abbe91.svg"
+  },
+  {
+    "revision": "85885fc533e4d9c2ebc7bcfc67a86456",
+    "url": "./static/media/avatar.85885fc5.jpg"
+  },
+  {
+    "revision": "97f1dc8124ed86ff8c6860891b5abe53",
+    "url": "./static/media/bubbleTip.97f1dc81.svg"
+  },
+  {
+    "revision": "bafe520a5416d26ef58ebc31e55d1ce1",
+    "url": "./static/media/call.bafe520a.svg"
+  },
+  {
+    "revision": "51a2b4f4e26018b639fe1f668d05b2b1",
+    "url": "./static/media/camera.51a2b4f4.svg"
+  },
+  {
+    "revision": "32b06707654757489c1a76d52735a0a6",
+    "url": "./static/media/caret_down.32b06707.svg"
+  },
+  {
+    "revision": "9653d13fe84a63402581ccaeb11cbb3e",
+    "url": "./static/media/clip.9653d13f.svg"
+  },
+  {
+    "revision": "1f12d769c44e8606b6e224b1de24980e",
+    "url": "./static/media/closeCircle.1f12d769.svg"
+  },
+  {
+    "revision": "09dfd700547f4a064cc59d301410088d",
+    "url": "./static/media/cross.09dfd700.svg"
+  },
+  {
+    "revision": "5f9db8d1347dccdd2be6dfc6cd6ce1ce",
+    "url": "./static/media/fileExcel.5f9db8d1.svg"
+  },
+  {
+    "revision": "22ff85a3cd7ed714a3612c075deeebe8",
+    "url": "./static/media/fileImage.22ff85a3.svg"
+  },
+  {
+    "revision": "228ef714c47ec29240c600af9d7f56e9",
+    "url": "./static/media/filePdf.228ef714.svg"
+  },
+  {
+    "revision": "c63ce2a31d475404de6ce472aa947c26",
+    "url": "./static/media/filePpt.c63ce2a3.svg"
+  },
+  {
+    "revision": "ca2439ce11d3ff515b97607984bba597",
+    "url": "./static/media/fileWord.ca2439ce.svg"
+  },
+  {
+    "revision": "90ab8d28388f8ff419cf5e4b65d188be",
+    "url": "./static/media/fileZip.90ab8d28.svg"
+  },
+  {
+    "revision": "508a3b586197182a66791108f22ca0a1",
+    "url": "./static/media/microphone.508a3b58.svg"
+  },
+  {
+    "revision": "916456f15372afb76511daaa6a1f2d05",
+    "url": "./static/media/options.916456f1.svg"
+  },
+  {
+    "revision": "95923f046fcf4f5e4111fcd51a83639d",
+    "url": "./static/media/paper-1.95923f04.jpg"
+  },
+  {
+    "revision": "f5f491066a334c31a85e558173ad6703",
+    "url": "./static/media/paper-2.f5f49106.jpg"
+  },
+  {
+    "revision": "3c6e94d6c0e0decbd4da0302642f7ffc",
+    "url": "./static/media/paper-3.3c6e94d6.jpg"
+  },
+  {
+    "revision": "c22195c46ccd1c2e5ca27a80cb9231f9",
+    "url": "./static/media/paper-7.c22195c4.jpg"
+  },
+  {
+    "revision": "d73b324f4ef01652c01b72d0d922a31d",
+    "url": "./static/media/plane.d73b324f.svg"
+  },
+  {
+    "revision": "2f5697739f63873a37706767751262a5",
+    "url": "./static/media/play.2f569773.svg"
+  },
+  {
+    "revision": "a5bf319c8eb4ce176bb54eb81891973a",
+    "url": "./static/media/plus.a5bf319c.svg"
+  },
+  {
+    "revision": "e093edba73601af9fc76ffa412db5dc0",
+    "url": "./static/media/replied.e093edba.svg"
+  },
+  {
+    "revision": "aaf09d5245d68fc9eda45bcfcdb534db",
+    "url": "./static/media/search.aaf09d52.svg"
+  },
+  {
+    "revision": "3154514f8a4fb3cca8dfbaf45f8bbc3f",
+    "url": "./static/media/smile.3154514f.svg"
+  },
+  {
+    "revision": "6e0d3817a83e61ad77a41b3a2905a860",
+    "url": "./static/media/wave.6e0d3817.svg"
+  }
+]);
