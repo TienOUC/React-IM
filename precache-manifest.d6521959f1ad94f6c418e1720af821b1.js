e96354b6a8201ff8c8e0ce81209f27c6864c7d2f
tienouc <willtien@outlook.com>
2020-07-31 16:51:38 +0800

    Updates

---
+++ precache-manifest.d6521959f1ad94f6c418e1720af821b1.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219a3454999391df17b8f69701d11f4d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbaf7218c760a15f864d",
+    "url": "./static/css/main.86b25147.chunk.css"
+  },
+  {
+    "revision": "c4156158e3875ac6e089",
+    "url": "./static/js/2.ff90a61b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ff90a61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbaf7218c760a15f864d",
+    "url": "./static/js/main.91d1881e.chunk.js"
+  },
+  {
+    "revision": "16671eae8c676093c1e8",
+    "url": "./static/js/runtime-main.a58ab876.js"
+  },
+  {
+    "revision": "4eb0a6538a69e1698c7988089a8ad6d0",
+    "url": "./static/media/arrowMenuLeft.4eb0a653.svg"
+  },
+  {
+    "revision": "f9abbe91a3da1a97d72410561493e1aa",
+    "url": "./static/media/arrowRight.f9abbe91.svg"
+  },
+  {
+    "revision": "85885fc533e4d9c2ebc7bcfc67a86456",
+    "url": "./static/media/avatar.85885fc5.jpg"
+  },
+  {
+    "revision": "97f1dc8124ed86ff8c6860891b5abe53",
+    "url": "./static/media/bubbleTip.97f1dc81.svg"
+  },
+  {
+    "revision": "bafe520a5416d26ef58ebc31e55d1ce1",
+    "url": "./static/media/call.bafe520a.svg"
+  },
+  {
+    "revision": "51a2b4f4e26018b639fe1f668d05b2b1",
+    "url": "./static/media/camera.51a2b4f4.svg"
+  },
+  {
+    "revision": "32b06707654757489c1a76d52735a0a6",
+    "url": "./static/media/caret_down.32b06707.svg"
+  },
+  {
+    "revision": "9653d13fe84a63402581ccaeb11cbb3e",
+    "url": "./static/media/clip.9653d13f.svg"
+  },
+  {
+    "revision": "1f12d769c44e8606b6e224b1de24980e",
+    "url": "./static/media/closeCircle.1f12d769.svg"
+  },
+  {
+    "revision": "09dfd700547f4a064cc59d301410088d",
+    "url": "./static/media/cross.09dfd700.svg"
+  },
+  {
+    "revision": "5f9db8d1347dccdd2be6dfc6cd6ce1ce",
+    "url": "./static/media/fileExcel.5f9db8d1.svg"
+  },
+  {
+    "revision": "22ff85a3cd7ed714a3612c075deeebe8",
+    "url": "./static/media/fileImage.22ff85a3.svg"
+  },
+  {
+    "revision": "228ef714c47ec29240c600af9d7f56e9",
+    "url": "./static/media/filePdf.228ef714.svg"
+  },
+  {
+    "revision": "c63ce2a31d475404de6ce472aa947c26",
+    "url": "./static/media/filePpt.c63ce2a3.svg"
+  },
+  {
+    "revision": "ca2439ce11d3ff515b97607984bba597",
+    "url": "./static/media/fileWord.ca2439ce.svg"
+  },
+  {
+    "revision": "90ab8d28388f8ff419cf5e4b65d188be",
+    "url": "./static/media/fileZip.90ab8d28.svg"
+  },
+  {
+    "revision": "508a3b586197182a66791108f22ca0a1",
+    "url": "./static/media/microphone.508a3b58.svg"
+  },
+  {
+    "revision": "916456f15372afb76511daaa6a1f2d05",
+    "url": "./static/media/options.916456f1.svg"
+  },
+  {
+    "revision": "3e456138de3b1c248b1baf830d5378cc",
+    "url": "./static/media/paper-1.3e456138.jpg"
+  },
+  {
+    "revision": "f2f511dafc8414b29c05de92382055e3",
+    "url": "./static/media/paper-2.f2f511da.jpg"
+  },
+  {
+    "revision": "41af7d85d00799b376d3fa9ecc4c6baf",
+    "url": "./static/media/paper-3.41af7d85.jpg"
+  },
+  {
+    "revision": "5c5ed7fa09d0d769aadbfb22c07708b6",
+    "url": "./static/media/paper-7.5c5ed7fa.jpg"
+  },
+  {
+    "revision": "d73b324f4ef01652c01b72d0d922a31d",
+    "url": "./static/media/plane.d73b324f.svg"
+  },
+  {
+    "revision": "2f5697739f63873a37706767751262a5",
+    "url": "./static/media/play.2f569773.svg"
+  },
+  {
+    "revision": "a5bf319c8eb4ce176bb54eb81891973a",
+    "url": "./static/media/plus.a5bf319c.svg"
+  },
+  {
+    "revision": "e093edba73601af9fc76ffa412db5dc0",
+    "url": "./static/media/replied.e093edba.svg"
+  },
+  {
+    "revision": "aaf09d5245d68fc9eda45bcfcdb534db",
+    "url": "./static/media/search.aaf09d52.svg"
+  },
+  {
+    "revision": "3154514f8a4fb3cca8dfbaf45f8bbc3f",
+    "url": "./static/media/smile.3154514f.svg"
+  },
+  {
+    "revision": "6e0d3817a83e61ad77a41b3a2905a860",
+    "url": "./static/media/wave.6e0d3817.svg"
+  }
+]);
