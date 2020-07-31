15c98b8ada979704dfb616bc24f500f6455041a4
tienouc <willtien@outlook.com>
2020-08-01 00:18:42 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5265212ce5d13894a2a7093a770da6fc.js"
+  "./precache-manifest.e322158101cff6b58cc52ee3de48fbdc.js"
 );
 
 self.addEventListener('message', (event) => {
